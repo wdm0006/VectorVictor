71751f66d698b1ca7ea4c490f274091696a8c497
Will McGinnis <will@predikto.com>
2016-11-02 19:45:26 -0400

    some gradual updates, middleware not working yet

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
     	"github.com/gin-gonic/gin"
 	"strconv"
-	"strings"
 	"time"
 	"./norms"
 	"./coersion"
 	"fmt"
 	"errors"
 	"net/http"
+	"strings"
 )
 
 // Wrap response is a helper function to wrap the content block of
@@ -24,6 +24,24 @@ func wrap_response(content gin.H, errors error) (gin.H){
 	}
 	return response
 }
+
+// a middleware for adding in the start time of the request
+func TimerMiddleWare() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// before request
+		t := time.Now()
+
+		// process request
+		c.Next()
+
+		// after request
+		latency := time.Since(t)
+		// TODO: append this latency to the response itself
+		print(latency.String())
+	}
+}
+
+
 
 // Index simply responds with an indication that the system is up.
 func index (c *gin.Context){
@@ -55,15 +73,16 @@ func square (c *gin.Context){
 // norm takes the l2 norm of a vector
 func norm (c *gin.Context) {
 	// parse the input into a vector of floats
-	stringvec := c.Param("v")
-	kind := "l2"
+	stringvec := c.Query("v")
+	kind := c.DefaultQuery("kind", "l2")
+	kind = strings.ToLower(kind)
 
 	arr, err := coersion.CSV2FloatArray(stringvec)
 
 	var response gin.H
 	var code int
 	if err != nil {
-		content := gin.H{"vector": arr, "norm": nil}
+		content := gin.H{"vector": arr, "norm": nil, "kind": kind}
 		response = wrap_response(content, err)
 		code = 500
 	} else {
@@ -71,17 +90,21 @@ func norm (c *gin.Context) {
 		var err error
 		if kind == "l2" {
 			norm, err = norms.L2(arr)
+		} else if kind == "l1"{
+			norm, err = norms.L1(arr)
+		} else if kind == "linfinity" {
+			norm, err = norms.Linfinity(arr)
 		} else {
 			err = errors.New(fmt.Sprintf("unknown norm kind: %s", kind))
 		}
 
 
 		if err != nil {
-			content := gin.H{"vector": arr, "norm": norm}
+			content := gin.H{"vector": arr, "norm": norm, "kind": kind}
 			response = wrap_response(content, err)
 			code = 500
 		} else {
-			content := gin.H{"vector": arr, "norm": norm}
+			content := gin.H{"vector": arr, "norm": norm, "kind": kind}
 			response = wrap_response(content, nil)
 			code = 200
 		}
@@ -93,7 +116,13 @@ func norm (c *gin.Context) {
 
 
 func main(){
-	app := gin.Default()
+	app := gin.New()
+
+	// middlwares
+	app.Use(gin.Logger())
+	app.Use(gin.Recovery())
+	app.Use(TimerMiddleWare())
+
 	app.LoadHTMLGlob("templates/**/*")
 
 	// index page, post returns json status, get will return an html status page
@@ -113,8 +142,8 @@ func main(){
 	})
 
 	// norm: a post will return json response and get will be an html page
-	app.POST("/norm/:v", norm)
-	app.GET("/norm/:v", func(c *gin.Context) {
+	app.POST("/norm", norm)
+	app.GET("/norm", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "public/norms.tmpl", gin.H{
 			"title": "hello world",
 		})
--- norms/norms.go
+++ norms/norms.go
@@ -4,23 +4,63 @@ import (
 	"math"
 )
 
-// l2 norm is the sqrt of sum of squares of a vector TODO: any exception
-// handling whatsoever.
-func L2(arr []float64) (float64, error){
-	// square all elements of the array
-	sqrs := []float64{}
+// the max N value supported for exponents
+const MAX_N float64 = 100.0
+
+
+func array_min(arr []float64) (float64, error) {
+	smallest := arr[0]
 	for _, v := range arr {
-		sqrs = append(sqrs, v * v)
+		if v < smallest {
+			smallest = v
+		}
 	}
+	return smallest, nil
+}
 
-	// sum it
-	var sum_squared = 0.0
-	for _, v := range sqrs {
-		sum_squared += v
+func array_max(arr []float64) (float64, error) {
+	largest := arr[0]
+	for _, v := range arr {
+		if v > largest {
+			largest = v
+		}
 	}
+	return largest, nil
+}
 
-	// take the square root
-	l2_norm := math.Sqrt(sum_squared)
+func LN(arr []float64, N float64) (float64, error){
+	// we put a max value on N and assume L-infinity above that
+	if N >= MAX_N {
+		return array_max(arr)
+	} else {
+		// square all elements of the array
+		sqrs := []float64{}
+		for _, v := range arr {
+			sqrs = append(sqrs, math.Pow(v, N))
+		}
 
-	return l2_norm, nil
+		// sum it
+		var sum_squared = 0.0
+		for _, v := range sqrs {
+			sum_squared += v
+		}
+
+		// take the square root
+		norm := math.Pow(sum_squared, 1.0 / N)
+
+		return norm, nil
+	}
+}
+
+// l2 norm is the sqrt of sum of squares of a vector
+func L2(arr []float64) (float64, error){
+	return LN(arr, 2.0)
+}
+
+func L1(arr []float64) (float64, error){
+	return LN(arr, 1.0)
+}
+
+func Linfinity(arr []float64) (float64, error){
+	return LN(arr, MAX_N + 1)
 }
